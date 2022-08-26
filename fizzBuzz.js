6810a92bae77d18141f5c7a888c1c4672aef1abc
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-08-25 23:43:57 -0500

    Added let keyword

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,6 @@
 // Write your solution below this line:
 
-for (i = 1; i <= 50; i++) {
+for (let i = 1; i <= 50; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("fizzbuzz");
     } else if (i % 3 === 0) {
